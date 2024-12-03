c347132ecc580fcef3c3888f136d81cc768d7cba
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-12-02 20:59:24 -0800

    Day two: working (but slow) solution

--- src/year_2024/day_two/mod.rs
+++ src/year_2024/day_two/mod.rs
@@ -21,7 +21,7 @@ const _INPUT: &str = "7 6 4 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9";
-const ACCEPTANCE_DIFFERENCE: std::ops::RangeInclusive<u32> = 1..=3;
+const ACCEPTABLE_DIFFERENCE: std::ops::RangeInclusive<u32> = 1..=3;
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 enum Report {
@@ -29,19 +29,9 @@ enum Report {
     Unsafe,
 }
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-enum TolerantReport {
-    Safe,
-    SingleUnsafe,
-    DoubleUnsafe,
-}
-
-impl TolerantReport {
-    pub fn advance(&mut self) {
-        *self = match self {
-            Self::Safe => Self::SingleUnsafe,
-            _ => Self::DoubleUnsafe,
-        };
+impl Report {
+    pub fn is_safe(&self) -> bool {
+        matches!(self, Self::Safe)
     }
 }
 
@@ -96,39 +86,61 @@ pub fn part_one() -> u32 {
         .unwrap()
 }
 
-// This does not currently work for the full data set.
+// This works, but could definitely do with some optimization.
 pub fn part_two() -> u32 {
-    fn evaluate(report: &Vec<u32>) -> (TolerantReport, Option<usize>) {
-        let mut previous = None;
-        let mut direction = None;
-        let mut safe = TolerantReport::Safe;
-        let mut first_unsafe_index = None;
+    fn evaluate_direction(report: &[u32]) -> Option<Direction> {
+        // Could probably find offending indicies using `.find()`
+        if report.is_sorted() {
+            return Some(Direction::Increasing);
+        }
+        if report.iter().rev().is_sorted() {
+            return Some(Direction::Decreasing);
+        }
+        None
+    }
 
-        for (index, level) in report.iter().enumerate() {
-            let Some(last) = previous else {
-                previous = Some(level);
+    fn evaluate_stepping(report: &[u32]) -> Report {
+        let mut prev = None;
+        for value in report.iter() {
+            let Some(last) = prev else {
+                prev = Some(value);
                 continue;
             };
 
-            if verify_difference(*last, *level) == Report::Unsafe
-                || verify_or_set_direction(*last, *level, &mut direction) == Report::Unsafe
-            {
-                print!("   ╭─ {report:?} at {level}\n   ╰─ {safe:?} -> ");
-                safe.advance();
-                println!("{safe:?}\n");
-                match safe {
-                    TolerantReport::SingleUnsafe => first_unsafe_index = Some(index),
-                    TolerantReport::DoubleUnsafe => break,
-                    _ => (),
-                }
+            if !ACCEPTABLE_DIFFERENCE.contains(&last.abs_diff(*value)) {
+                println!(
+                    "Diff: unsafe at {value} (diff {}): {report:?}",
+                    last.abs_diff(*value)
+                );
+                return Report::Unsafe;
             }
 
-            previous = Some(level);
+            prev = Some(value);
         }
 
-        println!("╭─ {report:?}\n╰─ {safe:?}\n");
+        println!("Diff: safe: {report:?}");
+        Report::Safe
+    }
 
-        (safe, first_unsafe_index)
+    fn evaluate(report: &[u32]) -> Report {
+        if evaluate_direction(report).is_some() && evaluate_stepping(report).is_safe() {
+            Report::Safe
+        } else {
+            Report::Unsafe
+        }
+    }
+
+    fn delete_until_safe(report: &[u32]) -> Report {
+        for i in 0..report.len() {
+            let mut report = report.to_owned();
+            report.remove(i);
+
+            if evaluate(report.as_slice()).is_safe() {
+                return Report::Safe;
+            }
+        }
+
+        Report::Unsafe
     }
 
     let reports = parse(INPUT);
@@ -136,19 +148,11 @@ pub fn part_two() -> u32 {
     reports
         .into_iter()
         .filter(|report| {
-            let (safe, unsafe_index) = evaluate(report);
-            match safe {
-                TolerantReport::Safe => true,
-                TolerantReport::SingleUnsafe => {
-                    println!("# Retrying {report:?} ({safe:?})\n");
-                    let mut report = report.clone();
-                    report.remove(unsafe_index.unwrap());
-                    let result = evaluate(&report).0;
-                    println!("# Retried {report:?} ({result:?})\n");
-                    result == TolerantReport::Safe
-                }
-                TolerantReport::DoubleUnsafe => false,
+            let mut safe = evaluate(report);
+            if !safe.is_safe() {
+                safe = delete_until_safe(report);
             }
+            safe.is_safe()
         })
         .count()
         .try_into()
@@ -159,7 +163,7 @@ pub fn part_two() -> u32 {
 fn verify_difference(previous_level: u32, level: u32) -> Report {
     let difference = previous_level.max(level) - previous_level.min(level);
 
-    if !ACCEPTANCE_DIFFERENCE.contains(&difference) {
+    if !ACCEPTABLE_DIFFERENCE.contains(&difference) {
         Report::Unsafe
     } else {
         Report::Safe
