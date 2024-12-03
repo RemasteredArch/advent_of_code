7eb589e70c75c7b7a7af1111a8684858f9cbd736
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-12-03 11:13:54 -0800

    Day three, part one: finished implementation

--- src/year_2024/day_three/mod.rs
+++ src/year_2024/day_three/mod.rs
@@ -1,6 +1,6 @@
 const INPUT: &str = include_str!("./data.txt");
+const _INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
-// Not finished!
 pub fn part_one() -> u32 {
     const NUM_LEN: std::ops::RangeInclusive<usize> = 1..=3;
 
@@ -12,6 +12,36 @@ pub fn part_one() -> u32 {
         NumberOrCloseParen,
         Comma,
         None,
+    }
+
+    impl Pattern {
+        pub fn ideal_next(stack: &str) -> Self {
+            match stack.chars().last() {
+                Option::None => Pattern::Char('m'),
+                Some('m') => Pattern::Char('u'),
+                Some('u') => Pattern::Char('l'),
+                Some('l') => Pattern::Char('('),
+                Some('(') => Pattern::Number,
+                Some(c) if c.is_numeric() => {
+                    let is_lhs = !stack.contains(',');
+
+                    if current_num_length(stack) == *NUM_LEN.end() {
+                        if is_lhs {
+                            Pattern::Comma
+                        } else {
+                            Pattern::Char(')')
+                        }
+                    } else if is_lhs {
+                        Pattern::NumberOrComma
+                    } else {
+                        Pattern::NumberOrCloseParen
+                    }
+                }
+                Some(',') => Pattern::Number,
+                Some(')') => Pattern::None,
+                _ => panic!("bad character in stack"),
+            }
+        }
     }
 
     impl PartialEq<char> for Pattern {
@@ -32,7 +62,6 @@ pub fn part_one() -> u32 {
     }
 
     fn eval_stack(stack: String) -> u32 {
-        dbg!(&stack);
         let [lhs, rhs] = stack
             .split(',')
             .map(|str| {
@@ -50,49 +79,29 @@ pub fn part_one() -> u32 {
         lhs * rhs
     }
 
-    let mut stack = String::new();
+    // This doesn't need to be heap allocated, it has a fixed maximum length.
+    let mut stack = String::with_capacity("mul(123,123)".len());
     let mut total: u32 = 0;
 
     for char in INPUT.chars() {
-        print!("{char}");
-        let ideal_next = match stack.chars().last() {
-            None => Pattern::Char('m'),
-            Some('m') => Pattern::Char('u'),
-            Some('u') => Pattern::Char('l'),
-            Some('l') => Pattern::Char('('),
-            Some('(') => Pattern::Number,
-            Some(')') => Pattern::None,
-            Some(',') => Pattern::Number,
-            Some(c) if c.is_numeric() => {
-                let is_lhs = !stack.contains(',');
-                eprintln!("{}╰─ {c} is_lhs: {is_lhs}", " ".repeat(19 + " == ".len()));
+        // The expected value of the current character, based on the current top of the stack.
+        let mut ideal_next = Pattern::ideal_next(&stack);
 
-                if current_num_length(&stack) == *NUM_LEN.end() {
-                    if is_lhs {
-                        Pattern::Comma
-                    } else {
-                        Pattern::Char(')')
-                    }
-                } else if is_lhs {
-                    Pattern::NumberOrComma
-                } else {
-                    Pattern::NumberOrCloseParen
-                }
-            }
-            _ => panic!("bad character in stack"),
-        };
-        eprintln!(
-            "{:>19} == {char} {} ({stack:?})",
-            format!("{ideal_next:?}"),
-            ideal_next == char
-        );
-
+        // If no character is expected, that means the stack is finished and can be evaluated.
         if ideal_next == Pattern::None {
+            // Drain the stack and evaluate the result.
             total += eval_stack(std::mem::take(&mut stack));
+
+            // Reset the expectation for the newly-emptied stack.
+            ideal_next = Pattern::ideal_next(&stack);
         }
 
         if ideal_next == char {
+            // Current stack is still valid, add to it.
             stack.push(char);
+        } else {
+            // Current stack is invalid, clear it.
+            stack.clear();
         }
     }
 
